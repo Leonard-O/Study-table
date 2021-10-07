aafd9f5afc261e1da3dd468fb69efe400f50698a
Leonard <realcodedevs@gmail.com>
2021-10-07 15:27:36 +0300

    Study-time-Table

---
+++ table/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import SideBar from './SideBar';
+import Table from './Table';
+
+function App() {
+	return (
+		<div className="App">
+			<div className="container">
+				<Table />
+				<SideBar />
+			</div>
+		</div>
+	);
+}
+
+export default App;
---
+++ table/src/SideBar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './SideBar.css';
+
+function SideBar() {
+	return (
+		<div className="aside">
+			<div className="activities">
+				<p>Study</p>
+				<p>Eat</p>
+				<p>Travel</p>
+				<p>Sports</p>
+				<p>Gaming</p>
+				<p>Tv</p>
+				<p>Chill</p>
+				<p>Friends</p>
+				<p>Family</p>
+				<p>Pray</p>
+				<p>Work</p>
+				<p>Sleep</p>
+				<p>I'm blank</p>
+			</div>
+		</div>
+	);
+}
+
+export default SideBar;
